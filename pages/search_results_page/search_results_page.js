05a0c802adac4dffb13d67fa56fdde771e3680b4
Tien Dung <dung14b@gmail.com>
2023-11-21 20:48:46 +0700

    prevent cors policy bug (f*ck chromium)

--- pages/search_results_page/search_results_page.js
+++ pages/search_results_page/search_results_page.js
@@ -1,5 +1,6 @@
 const main_content = document.getElementById('main__content__result');
 let verticalFilterCurrent = 'filter_post';
+let currentPage = 'post_list';
 const page_num = document.getElementById('page-num');
 
 setTimeout(function () {
@@ -8,21 +9,22 @@ setTimeout(function () {
 
 document.getElementById('filter_post').classList.add('clicked');
 
-$.get('../../assets/posts.html', function (data) {
-  $('#main__content__result').html($(data));
-});
+document.getElementById(currentPage).style.visibility = 'visible';
 
 document.getElementById('filter_post').addEventListener('click', function () {
   if (verticalFilterCurrent == 'filter_post') return;
   page_num.style.visibility = 'visible';
   document.getElementById(verticalFilterCurrent).classList.remove('clicked');
-  document.getElementById('filter_post').classList.add('clicked');
+  document.getElementById(currentPage).classList.remove('loaded');
+  document.getElementById(currentPage).style.display = 'none';
   verticalFilterCurrent = 'filter_post';
-  $.get('../../assets/posts.html', function (data) {
-    $('#main__content__result').html($(data));
-  });
+  document.getElementById(verticalFilterCurrent).classList.add('clicked');
+  currentPage = 'post_list';
   setTimeout(function () {
-    document.getElementById('post_list').classList.add('loaded');
+    document.getElementById(currentPage).style.display = 'flex';
+    setTimeout(function () {
+      document.getElementById(currentPage).classList.add('loaded');
+    }, 100);
   }, 100);
 });
 
@@ -30,13 +32,16 @@ document.getElementById('filter_product').addEventListener('click', function () 
   if (verticalFilterCurrent == 'filter_product') return;
   page_num.style.visibility = 'visible';
   document.getElementById(verticalFilterCurrent).classList.remove('clicked');
+  document.getElementById(currentPage).classList.remove('loaded');
+  document.getElementById(currentPage).style.display = 'none';
   verticalFilterCurrent = 'filter_product';
   document.getElementById(verticalFilterCurrent).classList.add('clicked');
-  $.get('../../assets/products.html', function (data) {
-    $('#main__content__result').html($(data));
-  });
+  currentPage = 'products';
   setTimeout(function () {
-    document.getElementById('products').classList.add('loaded');
+    document.getElementById(currentPage).style.display = 'grid';
+    setTimeout(function () {
+      document.getElementById(currentPage).classList.add('loaded');
+    }, 100);
   }, 100);
 });
 
@@ -44,9 +49,16 @@ document.getElementById('filter_user').addEventListener('click', function () {
   if (verticalFilterCurrent == 'filter_user') return;
   page_num.style.setProperty('visibility', 'hidden');
   document.getElementById(verticalFilterCurrent).classList.remove('clicked');
+  document.getElementById(currentPage).classList.remove('loaded');
+  document.getElementById(currentPage).style.display = 'none';
   verticalFilterCurrent = 'filter_user';
   document.getElementById(verticalFilterCurrent).classList.add('clicked');
-  $.get('../../assets/users.html', function (data) {
-    $('#main__content__result').html($(data));
-  });
+  currentPage = 'users';
+  console.log(document.getElementById(currentPage));
+  setTimeout(function () {
+    document.getElementById(currentPage).style.display = 'flex';
+    setTimeout(function () {
+      document.getElementById(currentPage).classList.add('loaded');
+    }, 100);
+  }, 100);
 });
