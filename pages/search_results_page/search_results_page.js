6d33060b6514a285f40ca3b16eb34a591f49618e
Tien Dung <dung14b@gmail.com>
2023-12-02 06:44:47 +0700

    fix some ui

--- pages/search_results_page/search_results_page.js
+++ pages/search_results_page/search_results_page.js
@@ -2,3 +2,8 @@ function showText(item) {
   document.getElementById("post_relative").innerHTML = item.innerHTML;
   document.getElementById("post_relative").classList.add("clicked");
 }
+
+function showText2(item) {
+  document.getElementById("product_price").innerHTML = item.innerHTML;
+  document.getElementById("product_price").classList.add("clicked");
+}
