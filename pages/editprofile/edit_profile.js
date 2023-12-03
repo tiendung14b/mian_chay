1be966dbb51ff7207678399e88735c3b02e83d8a
Tien Dung <dung14b@gmail.com>
2023-12-03 23:42:51 +0700

    ok

--- pages/editprofile/edit_profile.js
+++ pages/editprofile/edit_profile.js
@@ -4,8 +4,15 @@ function onShowAddAddress() {
   console.log("show");
 }
 
+function onShowChangePhone() {
+  document.getElementById("change_phone").classList.add("show");
+  document.getElementById("main").classList.add("blur");
+  console.log("show");
+}
+
 function onBlur() {
   console.log("clicked");
   document.getElementById("add_address_popup").classList.remove("show");
+  document.getElementById("change_phone").classList.remove("show");
   document.getElementById("main").classList.remove("blur");
 }
