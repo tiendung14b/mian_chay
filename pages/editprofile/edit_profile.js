dddcbdc4560cf034f35a52b6fc9bbadf2671bfba
Tien Dung <dung14b@gmail.com>
2023-12-05 19:27:04 +0700

    ok

--- pages/editprofile/edit_profile.js
+++ pages/editprofile/edit_profile.js
@@ -26,12 +26,20 @@ function onShowChangePhone() {
   console.log("show");
 }
 
+function onShowResetPassword() {
+  document.getElementById("reset_pass_popup").classList.add("show");
+  document.getElementById("main").classList.add("blur");
+  document.getElementById("main").classList.add("total_blur");
+  console.log("show");
+}
+
 function onBlur() {
   console.log("clicked");
   document.getElementById("add_address_popup").classList.remove("show");
   document.getElementById("update_address_popup").classList.remove("show");
   document.getElementById("delete_address_popup").classList.remove("show");
   document.getElementById("change_phone").classList.remove("show");
+  document.getElementById("reset_pass_popup").classList.remove("show");
   document.getElementById("main").classList.remove("blur");
   document.getElementById("main").classList.remove("total_blur");
 }
